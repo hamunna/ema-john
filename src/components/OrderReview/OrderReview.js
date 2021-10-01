6539930d2c0dfd22110b81f3c1c1a8a9d56f5301
hamunna <hamunna@gmail.com>
2021-10-01 18:40:22 +0600

    Cart Problem Solved

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import useCart from '../Hooks/useCart';
-import useProducts from '../Hooks/useProducts';
+import useCart from '../../Hooks/useCart';
+import useProducts from '../../Hooks/useProducts';
 import Cart from '../Cart/Cart';
 import OrderItem from '../OrderItem/OrderItem';
+import { deleteFromDb } from '../../utilities/fakedb';
 
 const OrderReview = () => {
-	const [products, setProducts] = useProducts();
+	const [products] = useProducts();
 	const [cart, setCart] = useCart(products);
 
 	const handleRemove = key => {
 		const newCart = cart.filter(product => product.key !== key);
 		setCart(newCart);
+		deleteFromDb(key);
 	}
+
 	return (
 		<div>
 			
 			<div className="shop-container">
 				<div className="product-container">
 					{
-						products.map(product => <OrderItem
+						cart.map(product => <OrderItem
 							key={product.key}
 							product={product}
 							handleRemove={handleRemove}
